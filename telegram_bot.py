5eab37de3271f5942c4b796d18318456629ffa15
Leiver Moreno <leivermoreno@gmail.com>
2023-01-28 15:47:45 -0400

    removed logging

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,8 +1,3 @@
-import logging
-import logging.config
-import os
-from os import path
-
 import yaml
 from mercantil_automation.bank import Bank
 from telegram import Update
@@ -14,19 +9,10 @@ from telegram.ext import (
 )
 from utils import command_handler, message_handler
 
-environment = os.environ.get("PYTHON_ENV")
-if environment == "development":
-    path_to_file = path.abspath("logging.conf")
-    logging.config.fileConfig(path_to_file)
-
-logger = logging.getLogger("main")
-logger.debug("Logger successfully configured.")
-
 with open("./secret.yaml", "r", encoding="utf-8") as f:
     data = yaml.load(f, yaml.FullLoader)
     TOKEN = data["bot_token"]
     PASSCODE = data["passcode"]
-    logger.debug("Sensitive data loaded.")
 
 
 # command handlers
@@ -44,7 +30,6 @@ async def check_passcode_handler(update: Update, context: ContextTypes.DEFAULT_T
             raise ValueError("Invalid passcode.")
     except (IndexError, ValueError):
         passcode = context.args[0] if len(context.args) else None
-        logger.info(f"Failed passcode verification. Passcode: {passcode}.")
         await update.message.reply_text("Must provide a correct passcode.")
         raise ApplicationHandlerStop  # pylint: disable=raise-missing-from
     finally:
@@ -71,20 +56,10 @@ async def unknown_command_handler(update: Update, context: ContextTypes.DEFAULT_
 # catch edited commands to avoid being re-handled
 @message_handler(filters.UpdateType.EDITED_MESSAGE & filters.COMMAND)
 async def edited_message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logger.info(f"Caught updated command: {update.edited_message.text}")
     raise ApplicationHandlerStop
 
 
-# logs to console when a command is successfully handled, only in development
-@message_handler(filters.COMMAND)
-async def log_handled_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # get the command only
-    command = update.message.text.split(" ")[0]
-    logger.info(f'Command "{command}" successfully handled.')
-
-
 async def handle_error(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logger.exception(f"An error ocurred processing the update: {update.message.text}")
     await update.message.reply_text(
         "An error ocurred processing your command, please try again."
     )
@@ -99,11 +74,7 @@ def main():
         1: [start_handler, balance_handler, unknown_command_handler],
     }
     application.add_handlers(handlers)
-    if environment == "development":
-        application.add_handler(log_handled_command, 2)
-
     application.add_error_handler(handle_error)
-
     application.run_polling()
 
 
